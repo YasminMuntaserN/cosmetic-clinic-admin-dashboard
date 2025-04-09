8e891158b7025481d9dc29670e329a5eb575ddf6
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2025-04-09 22:53:38 +0200

    feat:add patient list and the patients page

---
+++ src/components/Patients/PatientList.tsx
@@ -0,0 +1,108 @@
+import {Column, Table} from "../ui/Table.tsx";
+import {Badge} from "../ui/Badge.tsx";
+import {Patient} from "../../types/patient.ts";
+
+
+interface PatientListProps {
+    Patients: Patient[];
+    totalCount: number;
+    onPageChange: (page: number) => void;
+    onSort: (field: string, ascending: boolean) => void;
+    currentPage: number;
+    pageSize: number;
+    isLoading?: boolean;
+}
+
+export function PatientList({
+                                Patients,
+                                totalCount,
+                                onPageChange,
+                                onSort,
+                                currentPage,
+                                pageSize,
+                                isLoading = false
+                            }: PatientListProps) {
+    const columns: Column<Patient>[] = [
+        {
+            key: 'firstName',
+            header: 'First Name',
+            sortable: true,
+        },
+        {
+            key: 'lastName',
+            header: 'Last Name',
+            sortable: true,
+        },
+        {
+            key: 'email',
+            header: 'Email',
+            sortable: true,
+        },
+        {
+            key: 'phone',
+            header: 'Phone',
+            sortable: false,
+        },
+        {
+            key: 'dateOfBirth',
+            header: 'Age',
+            sortable: false,
+            render: (patient) =>(patient.dateOfBirth && Math.floor((Date.now() - new Date(patient.dateOfBirth).getTime()) / (365.25 * 24 * 60 * 60 * 1000))) || 'N/A',
+        },
+        {
+            key: 'gender',
+            header: 'Gender',
+            sortable: false,
+            render: (Patient) => (
+                <Badge variant={Patient.gender === 'Male' ? 'Male' : 'Female'}>
+                    {Patient.gender === 'Male' ? 'Male' : 'Female'}
+                </Badge>
+            ),
+        },
+        {
+            key: 'address.city',
+            header: 'City',
+            sortable: false,
+            render: (patient) => patient.address?.city || 'N/A',
+        },
+        {
+            key: 'allergies',
+            header: 'Allergies',
+            sortable: false,
+            render: (patient) =>
+                patient.allergies.length > 0 ? (
+                    <div className="flex flex-wrap gap-1">
+                        {patient.allergies.map((allergy, index) => (
+                            <Badge key={index} variant='info'>
+                                {allergy}
+                            </Badge>
+                        ))}
+                    </div>
+                ) : (
+                    'No allergies'
+                ),
+        }
+    ];
+    const handleRowClick = (Patient: Patient) => {
+        console.log('Selected Patient:', Patient);
+        // Implement your row click handler
+    };
+
+    return (
+        <div className="p-6">
+            <h1 className="text-3xl font-slab mb-2">Patients</h1>
+            <Table
+                data={Patients}
+                columns={columns}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={onPageChange}
+                onRowClick={handleRowClick}
+                totalItems={totalCount}
+                isLoading={isLoading}
+                onSort={onSort}
+                className="shadow-sm"
+            />
+        </div>
+    );
+}
---
+++ src/pages/Patients.tsx
@@ -0,0 +1,82 @@
+import {useEffect, useState} from "react";
+import {PAGE_SIZE} from "../utils/constants.ts";
+import {usePaginatedPatientsList} from "../components/Patients/hooks/usePatient.ts";
+import {useSearchedPatients} from "../components/Patients/hooks/usePatient.ts";
+import {useClinic} from "../context/ClinicContext.tsx";
+import {PaginatedResponse} from "../types/Pagination.ts";
+import {Patient} from "../types/patient.ts";
+import {PatientList} from "../components/Patients/PatientList.tsx";
+import {AddModal} from "../components/ui/AddModal.tsx";
+import {Modal} from "../components/ui/Modal.tsx";
+import {AddEditPatientForm} from "../components/Patients/AddEditPatientForm.tsx";
+
+export function Patients() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [orderBy, setOrderBy] = useState('firstName');
+    const [ascending, setAscending] = useState(true);
+    const [patientList, setPatientList] = useState<Patient[]>([]);
+    const {getPaginatedPatientsList, totalCount, isLoading, error} = usePaginatedPatientsList();
+    const {search} = useClinic();
+    const {
+        getSearchedPatients,
+        isLoading: SearchedPatientsLoading,
+        error: SearchedPatientsError
+    } = useSearchedPatients();
+
+
+    useEffect(() => {
+        if (search.term) {
+            getSearchedPatients(
+                {field: "firstName", value: search.term},
+                {
+                    onSuccess: (data: Patient[]) => {
+                        setPatientList(data);
+                    }
+                }
+            );
+        }
+    }, [search]);
+
+    useEffect(() => {
+        if (!search.term) {
+            getPaginatedPatientsList(
+                {PageNumber: currentPage, PageSize: PAGE_SIZE, OrderBy: orderBy, Ascending: ascending},
+                {
+                    onSuccess: (data: PaginatedResponse) => {
+                        setPatientList(data.data);
+                    }
+                }
+            );
+        }
+    }, [currentPage, orderBy, search.term]);
+
+    const handleSort = (field: string, isAscending: boolean) => {
+        setOrderBy(field);
+        setAscending(isAscending);
+        setCurrentPage(1);
+    };
+
+    if (error || SearchedPatientsError) return <p>Something get wrong</p>
+
+    return (
+        <Modal>
+            <div className="container mx-auto">
+                <PatientList
+                    Patients={patientList || []}
+                    totalCount={totalCount ?? 0}
+                    onPageChange={setCurrentPage}
+                    onSort={handleSort}
+                    currentPage={currentPage}
+                    pageSize={PAGE_SIZE}
+                    isLoading={isLoading || SearchedPatientsLoading}
+                />
+            </div>
+            <Modal.Open opens="addPatient">
+                <AddModal/>
+            </Modal.Open>
+            <Modal.Window name="addPatient">
+                <AddEditPatientForm />
+            </Modal.Window>
+        </Modal>
+    );
+}
