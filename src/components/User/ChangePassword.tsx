1b38db5074f54d8b8572a11443407940921f0877
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2025-04-12 21:45:03 +0300

    feat: Add useMediaQuery custom hook for responsive design
    - Implemented a reusable hook to detect media query matches (e.g., max-width breakpoints)
    - Ensures SSR compatibility by checking window availability
    - Listens to changes and updates on resize
    - Useful for handling responsive layout logic in React components

---
+++ src/components/User/ChangePassword.tsx
@@ -0,0 +1,82 @@
+import TextInput from "../ui/TextInput.tsx";
+import {useForm} from "react-hook-form";
+import {Button} from "../ui/Button.tsx";
+import {useChangePassword} from "./useUser.ts";
+import toast from "react-hot-toast";
+
+type PasswordForm = {
+    currentPassword: string;
+    password: string;
+    confirmPassword: string;
+};
+
+export function ChangePassword({userId}: { userId: string | undefined }) {
+    const {
+        control,
+        handleSubmit,
+        watch,
+        reset
+    } = useForm<PasswordForm>();
+    const { getChangePassword, isLoading, error} =useChangePassword();
+
+    const onSubmit = (data: PasswordForm) => {
+        if(userId)
+        getChangePassword({id:userId , currentPassword :data.currentPassword ,newPassword :data.password} ,{
+            onSuccess:()=> toast.success("Password changed successfully"),
+            onError:()=> toast.error("Password changed failed"),
+        })
+        reset();
+    };
+
+    const password = watch("password");
+
+    if(error) return <p>something get wrong..</p>
+    if(isLoading) return <p>Loading...</p>
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
+            <h2 className="text-2xl font-semibold">Change Password</h2>
+
+            <TextInput
+                control={control}
+                label="Current Password"
+                name="currentPassword"
+                placeholder="Current Password"
+                value="Password"
+            />
+            
+            <TextInput
+                control={control}
+                type="password"
+                label="New Password"
+                name="password"
+                rules={{
+                    required: "Password is required",
+                    minLength: {
+                        value: 8,
+                        message: "Password must be at least 8 characters"
+                    },
+                    pattern: {
+                        value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$/,
+                        message: "Password must include uppercase, lowercase, and a number"
+                    }
+                }}
+            />
+
+            <TextInput
+                control={control}
+                type="password"
+                label="Confirm Password"
+                name="confirmPassword"
+                rules={{
+                    required: "Please confirm your password",
+                    validate: value =>
+                        value === password || "Passwords do not match"
+                }}
+            />
+
+            <Button type="submit">Save Changes</Button>
+        </form>
+    );
+}
+
+
